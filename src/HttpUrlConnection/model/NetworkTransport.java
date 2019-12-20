ecdef5f10265f7e77ea91b21c3a158b182ec6cd0
Perrault <lucas.perrault@hotmail.com>
2019-12-21 00:01:07 +0100

    UPDATE [BACK] Stop[] to ArrayList<Stop>

--- src/HttpUrlConnection/model/NetworkTransport.java
+++ src/HttpUrlConnection/model/NetworkTransport.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class NetworkTransport {
     private Line[] _lines;
-    private Stop[] _stops;
+    private ArrayList<Stop> _stops;
     private ArrayList<Correspondence> _correspondences;
 
-    public NetworkTransport(Line[] lines, Stop[] stops, ArrayList<Correspondence> correspondences)
+    public NetworkTransport(Line[] lines, ArrayList<Stop> stops, ArrayList<Correspondence> correspondences)
     {
         _lines = lines;
         _stops = stops;
@@ -19,7 +19,7 @@ public class NetworkTransport {
         return _lines;
     }
 
-    public Stop[] getStops()
+    public ArrayList<Stop> getStops()
     {
         return _stops;
     }
@@ -31,7 +31,7 @@ public class NetworkTransport {
 
     @Override
     public String toString() {
-        return "The Network contains " + _lines.length + " lines which are " + _lines + " and " + _stops.length + " stops which are "
+        return "The Network contains " + _lines.length + " lines which are " + _lines + " and " + _stops.size() + " stops which are "
                 + _stops + " and correspondences " + _correspondences + "(" + _correspondences.size() + ").";
     }
 }
