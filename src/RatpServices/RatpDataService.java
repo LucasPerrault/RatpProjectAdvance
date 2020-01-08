0d42836a9bd6aa4d1d2ecba24d7980ce2faf2a87
Perrault <lucas.perrault@hotmail.com>
2020-01-08 17:12:39 +0100

    DELETE [BACK] we don't need the get line method

--- src/RatpServices/RatpDataService.java
+++ src/RatpServices/RatpDataService.java
@@ -17,27 +17,7 @@ public class RatpDataService
     public RatpDataService(String GET_URL, String USER_AGENT) {
         _httpClient = new HttpUrlConnection(GET_URL, USER_AGENT);
     }
-    // Get list of all ratp's lines
-    public ArrayList<Line> getRatpLines() throws IOException, ParseException
-    {
-        // Init result
-        ArrayList<Line> lines = new ArrayList<Line>();
-
-        // Init all data of ratp
-        JSONObject ratpData = _httpClient.getJsonObjectOfAllRatpData();
-
-        // Get Lines in JsonObject
-        JSONObject linesObject = (JSONObject)ratpData.get("lignes");
-        // Read all keys of lines
-        Iterator<String> keys = linesObject.keySet().iterator();
-
-        while(keys.hasNext()) {
-            String key = keys.next();
-            lines.add(ApiDataHelpers.castObjectToRatpLine((JSONObject)linesObject.get(key)));
-        }
-
-        return lines;
-    }
+   
     /* model => ["lines","stations" ]*/
     public  ArrayList getRatpData(String model) throws  IOException,ParseException {
         // Init result
