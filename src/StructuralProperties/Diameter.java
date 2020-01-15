813181670337fc7f13331a15c83b9e79eae7bcdf
Perrault <lucas.perrault@hotmail.com>
2020-01-15 19:27:24 +0100

    UPDATE in recursive

--- src/StructuralProperties/Diameter.java
+++ src/StructuralProperties/Diameter.java
@@ -7,7 +7,7 @@ import GraphAlgorithms.WeightedAlgorithmGraph.Dijkstra;
 import HttpUrlConnection.model.NetworkTransport;
 import HttpUrlConnection.model.Stop;
 
-import java.util.Optional;
+import java.util.*;
 
 public class Diameter extends AbstractStructuralPropertie
 {
@@ -16,51 +16,61 @@ public class Diameter extends AbstractStructuralPropertie
     public Diameter(NetworkTransport networkTransport)
     {
         _ratpFactory.inputGraphTypeForEccentricity();
+        Queue<Stop> stopsQueue = new LinkedList<>(networkTransport.getStops());
+
         int index = _ratpFactory._indexOfGraphType;
         switch (index) {
             case(1):
-                unweightedGraphDiameter(networkTransport);
+                unweightedGraphDiameter(networkTransport, stopsQueue);
                 break;
             case(2):
-                weightedGraphDiameter(networkTransport);
+                weightedGraphDiameter(networkTransport, stopsQueue);
                 break;
         }
     }
 
-    private void weightedGraphDiameter(NetworkTransport networkTransport)
+    private void weightedGraphDiameter(NetworkTransport networkTransport, Queue<Stop> stopQueue)
     {
 
-        for (Stop stop: networkTransport.getStops())
-        {
-            Eccentricity eccentricity = new Eccentricity(
-                    networkTransport,
-                    stop,
-                    Optional.of(new Dijkstra(stop, networkTransport, Optional.empty()))
-            );
-
-            if (_lengthPath < eccentricity.getLength())
-            {
-                _lengthPath = eccentricity.getLength();
-                _path = eccentricity.getPath();
-            }
+        if (stopQueue.isEmpty()) {
+            return;
         }
+
+        Stop stop = stopQueue.poll();
+
+        Eccentricity eccentricity = new Eccentricity(
+                networkTransport,
+                stop,
+                Optional.of(new Dijkstra(stop, networkTransport, Optional.empty()))
+        );
+
+        if (_lengthPath < eccentricity.getLength())
+        {
+            _lengthPath = eccentricity.getLength();
+            _path = eccentricity.getPath();
+        }
+        weightedGraphDiameter(networkTransport, stopQueue);
     }
 
-    private void unweightedGraphDiameter(NetworkTransport networkTransport)
+    private void unweightedGraphDiameter(NetworkTransport networkTransport, Queue<Stop> stopQueue)
     {
-        for (Stop stop: networkTransport.getStops())
-        {
-            Eccentricity eccentricity = new Eccentricity(
-                    networkTransport,
-                    stop,
-                    Optional.of(new BreadthFirstSearch(stop, networkTransport))
-            );
-
-            if (_lengthPath < eccentricity.getLength())
-            {
-                _lengthPath = eccentricity.getLength();
-                _path = eccentricity.getPath();
-            }
+        if (stopQueue.isEmpty()) {
+            return;
         }
+
+        Stop stop = stopQueue.poll();
+
+        Eccentricity eccentricity = new Eccentricity(
+                networkTransport,
+                stop,
+                Optional.of(new BreadthFirstSearch(stop, networkTransport))
+        );
+
+        if (_lengthPath < eccentricity.getLength())
+        {
+            _lengthPath = eccentricity.getLength();
+            _path = eccentricity.getPath();
+        }
+        unweightedGraphDiameter(networkTransport, stopQueue);
     }
 }
